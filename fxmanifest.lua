205b0dc109041b233a6f407c22beada5438a6d35
Guudz <Kilian1702@outlook.com>
2023-02-14 15:14:36 +0100

    Initial commit

---
+++ fxmanifest.lua
@@ -0,0 +1,13 @@
+fx_version "bodacious"
+
+games { "gta5" }
+
+this_is_a_map "yes"
+
+data_file "DLC_ITYP_REQUEST" "stream/meta/breze_cityhall.ytyp"
+
+files {
+  "stream/meta/breze_cityhall.ytyp"
+}
+
+-- Leaking Hub | J. Snow | https://leakinghub.com
